836d986bac0776547b8a7a8fce59eaf2f91a4186
Maryam Ghiasi <maryamghiasi2322@gmail.com>
2021-07-27 15:04:27 +0100

    fonte yekan added

--- src/components/BodyMenu.jsx
+++ src/components/BodyMenu.jsx
@@ -91,6 +91,7 @@ class BodyMenu extends React.Component {
             <div className='mainbody'>
                 <div className='sotoon'>
                     <Mycards
+                    className='yekan'
                         title='امروز در خیابان جمهوری تهران چه خبر بود؟'
                         content='معاون امنیتی استانداری: تجمع به خاطر قطعی برق در پاساژ علاءالدین بود / فارس: چندین نفر شعار سیاسی دادند / صداوسیما: جمعیت متفرق شد'
                         address='https://www.entekhab.ir/fa/news/629796/%D8%A7%D9%85%D8%B1%D9%88%D8%B2-%D8%AF%D8%B1-%D8%AE%DB%8C%D8%A7%D8%A8%D8%A7%D9%86-%D8%AC%D9%85%D9%87%D9%88%D8%B1%DB%8C-%D8%AA%D9%87%D8%B1%D8%A7%D9%86-%DA%86%D9%87-%D8%AE%D8%A8%D8%B1-%D8%A8%D9%88%D8%AF'
